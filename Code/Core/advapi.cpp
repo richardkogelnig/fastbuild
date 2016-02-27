2a91258bb992e50f713cd029652c225702506f07
richardk <richard.kogelnig@gmail.com>
2016-02-27 21:19:56 +0100

    fixes for dependency of advapi.dll

---
+++ Code/Core/advapi.cpp
@@ -0,0 +1,15 @@
+#include "Core\PrecompiledHeader.h"
+
+
+#pragma warning( push )
+#pragma warning( disable : 4100 )
+#pragma warning( disable : 6101 )
+ extern "C" {
+__declspec(dllexport) BOOLEAN WINAPI SystemFunction036(
+  _Out_ PVOID RandomBuffer,
+  _In_  ULONG RandomBufferLength
+){
+	return false;
+}
+}
+#pragma warning( pop ) 
